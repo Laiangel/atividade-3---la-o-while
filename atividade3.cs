55fe4bc8ddb93e2e4262d34dd8a883a0cf1692dd
Laiana angel <laianaangel57@gmail.com>
2024-12-06 14:26:37 -0300

    Add files via upload

---
+++ atividade3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class atividade3
+{
+    static void Main()
+    {
+        int contador = 10; // Inicializa o contador com 10
+
+        Console.WriteLine("Contagem regressiva:");
+
+        // Laço while que executa enquanto o contador for maior ou igual a 1
+        while (contador >= 1)
+        {
+            Console.WriteLine(contador); // Imprime o valor atual do contador
+            contador--; // Decrementa o valor do contador
+        }
+
+        Console.WriteLine("Fogo!");
+    }
+}
+
